c580aae0bb50171802abf09a2206e2d5620a535a
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-09-27 17:10:34 +0300

    coded the js part of the website

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 window.addEventListener('load', function(){
+    const btn=document.getElementById("btn");
+    const menu=document.getElementById("menu");
+    let n=0;
+
+    const move = ()=>{
+        if (n==0){
+            menu.className = "show-side-menu";
+            n=1;
+        }
+        else{
+            menu.className = "side-menu";
+            n=0;
+        }
+    }
+    btn.addEventListener("click",move);
 
 
 
