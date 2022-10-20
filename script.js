65e1185d70d03b47c4e61b6980af61596e3b7f76
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-10-20 19:30:13 +0300

    improve js file

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 window.addEventListener('load', function(){
     const btn=document.getElementById("btn");
     const menu=document.getElementById("menu");
-    let n=0;
 
     const move = ()=>{
-        if (n==0){
-            menu.className = "show-side-menu";
-            n=1;
-        }
-        else{
-            menu.className = "side-menu";
-            n=0;
-        }
+        menu.classList.toggle("show");
     }
     btn.addEventListener("click",move);
 
