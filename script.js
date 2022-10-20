db7c960734fe65504cdc5fcd10471c190cd6115c
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-10-20 19:50:11 +0300

    close btn is functional

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 window.addEventListener('load', function(){
     const btn=document.getElementById("btn");
     const menu=document.getElementById("menu");
+    const close=document.getElementById("close");
 
     const move = ()=>{
         menu.classList.toggle("show");
     }
     btn.addEventListener("click",move);
+    close.addEventListener("click",move);
 
 
 
